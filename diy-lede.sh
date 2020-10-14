0adcf087ebf77e47735f4d01dc4e00da79542585
gdck <nicknill@vip.qq.com>
2020-10-15 03:05:29 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -11,7 +11,7 @@
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 #sed -i 's/OpenWrt/OpenWrt-123/g' ./package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/LEDE/g' ./package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OpenWrt/g' ./package/base-files/files/bin/config_generate
 
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
@@ -39,9 +39,9 @@ git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci
 git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash  #clash出国软件
 git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan  #微信推送
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
-#git clone https://github.com/gdck/luci-app-adguardhome package/diy-packages/luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome #adguardhome广告过滤
-git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
+git clone https://github.com/gdck/luci-app-adguardhome package/lean/luci-app-adguardhome #adguardhome广告过滤
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome #adguardhome广告过滤
+#git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
 git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
